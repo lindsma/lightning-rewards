4d0351dd5813d9ce3e4364243b18e397570470b6
Lindsey Adams <linzma87@gmail.com>
2017-09-28 01:05:41 -0400

    Modal styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MdDialogModule, MdButtonModule, MdIconModule, MdAutocompleteModule, MdProgressBarModule } from '@angular/material';
+import { MdDialogModule, MdButtonModule, MdIconModule, MdAutocompleteModule, MdProgressBarModule, MdExpansionModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
@@ -33,6 +33,7 @@ import { RedeemRewardsComponent } from './dashboard/user-rewards-inventory/redee
     MdIconModule,
     MdAutocompleteModule,
     MdProgressBarModule,
+    MdExpansionModule,
     BrowserAnimationsModule
   ],
   entryComponents: [
