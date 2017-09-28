3f272a04a7ace07aac43a63a4f214aa6ec85fabb
RELIAS\wcoleman <wcoleman@reliaslearning.com>
2017-09-28 14:14:25 -0400

    button fix and conflict fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { UsersService } from './shared/users.service';
 
 @Component({
@@ -7,24 +7,37 @@ import { UsersService } from './shared/users.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
 
-  constructor( public usersService: UsersService) {}
+  welcomed = false;
+
+  constructor( public usersService: UsersService, private router: Router ) { }
+
+  ngOnInit() {
+  }
 
   login() {
-    let headers = {
-      'Content-Type': 'application/json'
-    }
+console.log('true');
+    const headers = {
+      'Content-Type': 'application/json',
+    };
 
     const user = {
       email: 'ladams@reliaslearning.com',
       password: 'training'
-    }
+    };
 
     this.usersService.login(user, headers).subscribe(res => {
       console.log(res);
+      this.welcomed = true;
+      this.router.navigate(['/dashboard']);
+
     });
-  };
+  }
+
+  logout() {
+    this.welcomed = false;
+  }
 
 }
