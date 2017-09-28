d085ae28d4fcf4f7ba5598f9f9f4ae4d80e9c611
Nick Anderson <nanderson@reliaslearning.com>
2017-09-28 17:25:34 -0400

    Finished wiring up the inbox modal

--- src/app/dashboard/reward-inbox/reward-inbox.component.ts
+++ src/app/dashboard/reward-inbox/reward-inbox.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MdDialog, MdDialogRef, MD_DIALOG_DATA } from '@angular/material';
+import { CardManagementService } from '../../shared/card-management.service';
 
 @Component({
   selector: 'app-reward-inbox',
@@ -9,11 +10,17 @@ import { MdDialog, MdDialogRef, MD_DIALOG_DATA } from '@angular/material';
 export class RewardInboxComponent implements OnInit {
 
   scratched = false;
+  currentCard: any;
+  headers = {
+    'Content-Type': 'application/json',
+  }
 
-  constructor(public dialogRef: MdDialogRef<RewardInboxComponent>, @Inject(MD_DIALOG_DATA) public data: any) { }
+  constructor(public dialogRef: MdDialogRef<RewardInboxComponent>, @Inject(MD_DIALOG_DATA) public data: any, public cardManagementService: CardManagementService) { }
 
   ngOnInit() {
-    console.log(this.data);
+    if(this.data.length > 0){
+      this.currentCard = this.data[0];
+    }
   }
 
   onCloseClick(): void {
@@ -22,6 +29,13 @@ export class RewardInboxComponent implements OnInit {
 
   scratch() {
     this.scratched = true;
+    this.cardManagementService.claimCard(this.currentCard.Id, this.headers).subscribe(res => {
+      console.log(true);
+    });
   }
 
+  nextCard() {
+    this.scratched = false;
+    this.currentCard = this.data[this.data.indexOf(this.currentCard) + 1];
+  }
 }
